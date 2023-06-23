2d0aa55735a1abe107d43c299ed4e83d3b1ae59d
harshitpandey0426 <harshitpandey0426@gmail.com>
2023-06-24 00:23:59 +0530

    Used paramaterised test

--- Junit-sample/src/test/SimpleCalculatorTest.java
+++ Junit-sample/src/test/SimpleCalculatorTest.java
@@ -1,15 +1,20 @@
 package test;
 
 import main.java.SimpleCalculator;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimpleCalculatorTest {
+//    @Before
+
+
     @Test
     void twoPlusTwoShouldBeFour(){
         SimpleCalculator calculator = new SimpleCalculator();
         int actual = calculator.add(2,2);
-        assertEquals(4,actual+1);
+        assertEquals(4,actual);
     }
 }
